4b025009ed67760824f0852b46ca8d1fc0c77d6a
Johnny <learnwithjohnny.official@gmail.com>
2023-03-06 18:07:40 +0200

    Update license in contracts

--- contracts/reentrancy-4/CryptoEmpireGame.sol
+++ contracts/reentrancy-4/CryptoEmpireGame.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: GPL-3.0-or-later 
 pragma solidity ^0.8.13;
 
 import { IERC1155 } from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
--- contracts/reentrancy-4/CryptoEmpireToken.sol
+++ contracts/reentrancy-4/CryptoEmpireToken.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: GPL-3.0-or-later 
 pragma solidity ^0.8.13;
 
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
